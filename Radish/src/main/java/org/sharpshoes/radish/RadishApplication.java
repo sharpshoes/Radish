fb6899a1fbe87ca6532634e0572d0aac39588d33
Casper <chongyuan.yang@gmail.com>
2018-02-26 22:43:33 +0800

    uu

---
+++ Radish/src/main/java/org/sharpshoes/radish/RadishApplication.java
@@ -0,0 +1,12 @@
+package org.sharpshoes.radish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadishApplication.class, args);
+	}
+}
