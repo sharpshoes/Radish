a82a827f95c063aadfd16d9a41e5d6f7fb558f8b
casper yang <yang_chongyuan@163.com>
2018-02-27 11:36:24 +0800

    init

--- src/main/java/org/sharpshoes/radish/RadishApplication.java
+++ src/main/java/org/sharpshoes/radish/RadishApplication.java
@@ -1,10 +1,18 @@
 package org.sharpshoes.radish;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+@ComponentScan({"org.sharpshoes.radish"})
+@EnableTransactionManagement
+@EnableAutoConfiguration
 @SpringBootApplication
-public class RadishApplication {
+public class RadishApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RadishApplication.class, args);
