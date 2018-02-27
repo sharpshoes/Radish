bd66c3107109cbfc8764bc010667a50f927fc369
Casper Yang <yang_chongyuan@163.com>
2018-02-27 17:39:03 +0800

    add sql file

--- src/main/java/org/sharpshoes/radish/entity/ApiEntity.java
+++ src/main/java/org/sharpshoes/radish/entity/ApiEntity.java
@@ -4,7 +4,7 @@ package org.sharpshoes.radish.entity;
 public class ApiEntity extends BaseEntity{
 
 	private Long id;
-	private String key;
+	private String code;
 	private String command;
 	
 	public Long getId() {
@@ -13,17 +13,17 @@ public class ApiEntity extends BaseEntity{
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getKey() {
-		return key;
-	}
-	public void setKey(String key) {
-		this.key = key;
-	}
 	public String getCommand() {
 		return command;
 	}
 	public void setCommand(String command) {
 		this.command = command;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
 	}
 	
 }
